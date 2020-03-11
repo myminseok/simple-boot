20b741e37e9ec26f3267102fbbf04c0eb49cf68e
MinSeok Kim <myminseok@gmail.com>
2020-03-11 23:19:06 +0900

    read env

--- src/main/java/com/example/simpleboot/SimpleBootApplication.java
+++ src/main/java/com/example/simpleboot/SimpleBootApplication.java
@@ -1,9 +1,11 @@
 package com.example.simpleboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
@@ -13,9 +15,27 @@ public class SimpleBootApplication {
 		SpringApplication.run(SimpleBootApplication.class, args);
 	}
 
+	@Value("${spring.application.name}")
+	String applicationName;
+
+	@Value("${cf.instance.index:default_from_class}")
+	String cfInstanceIndex;
+
+
+	@Value("${vcap.application.instance.id:default}")
+	String vcapApplicationInstanceId;
+
 	@GetMapping("/")
 	public String home() throws Exception{
-		return "hi";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("app: ").append(applicationName).append("<br>")
+				.append("cf.instance.index: ").append(cfInstanceIndex).append("<br>")
+		        .append("vcap.application.instance.id: ").append(vcapApplicationInstanceId).append("<br>");
+		return sb.toString();
 	}
+
+
 }
+
 
